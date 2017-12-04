f6216bdec2e027f2fba67c3fc6a7223ae4ef2917
kdurant <wangjun850725@163.com>
2017-12-04 16:51:03 +0800

    a little

--- udpCore.py
+++ udpCore.py
@@ -41,6 +41,7 @@ class UdpCore(QWidget):
         self.targetPort = QLineEdit('4444')
 
         self.bindRbtn = QRadioButton()
+        self.bindRbtn.setObjectName('singleRadioBtn')
 
         form = QFormLayout()
         form.addRow('本机IP地址：', self.masterIP)
@@ -71,7 +72,7 @@ class UdpCore(QWidget):
             QMessageBox.warning(self, "警告", 'UDP端口被占用')
 
     @pyqtSlot(str)
-    def sendUdpFrame(self, frame, dataMode='hex', boardCast=False):
+    def sendFrame(self, frame, dataMode='hex', boardCast=False):
         if dataMode == 'utf8':
             data = frame.encode(encoding='utf-8')
         elif dataMode == 'hex':
@@ -105,6 +106,7 @@ class UdpCore(QWidget):
                 self.udpSocket.readyRead.connect(self.processUDPDatagrams)
             else:
                 QMessageBox.warning(self, "警告", 'UDP端口被占用')
+                self.bindRbtn.setChecked(False)
         else:
             self.udpSocket.disconnectFromHost()
 
